a318015393079c3cd1cbfa6c1989258957b13d63
e-7281998 <jejjcs43@naver.com>
2022-02-13 12:19:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import "./style.css";
+
+
+const pj = [
+  {
+    id: 1, title: '고양이 가라사대',
+    site: "https://e-7281998.github.io/cat-jjal-maker/",
+    git: "https://github.com/e-7281998/cat-jjal-maker/tree/main/cat-jjal-maker-cra"
+  },
+  {
+    id: 2, title: '여행 사이트',
+    site: "https://e-7281998.github.io/portfolio_site/Private_holidays/",
+    git: "https://github.com/e-7281998/portfolio_site/tree/master/Private_holidays"
+  },
+  {
+    id: 3, title: '패션 사이트',
+    site: "https://e-7281998.github.io/portfolio_site/Avenue%20Fashion/",
+    git: "https://github.com/e-7281998/portfolio_site/tree/master/Avenue%20Fashion"
+  }
+]
+
+const pr = [
+  {
+    title: 'Git', site: "https://github.com/e-7281998",
+  },
+  {
+    title: 'Blog', site: "https://blog.naver.com/jejjcs43"
+  }
+]
+
+function onClick(e) {
+  e.preventDefault();
+  window.open(e.target.value);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {pj.map(pj =>
+          <div className='pj'>
+            <button className="title" key={pj.id} onClick={onClick} value={pj.site}>{pj.title}</button>
+            <button className="git" key={pj.id} onClick={onClick} value={pj.git}>코드 보러가기</button>
+          </div>)
+        }
+      </ul>
+      <ul className='prul'>
+        {pr.map(pr =>
+          <div className='pr'>
+            <button className="title" key={pr.title} onClick={onClick} value={pr.site}>{pr.title}</button>
+          </div>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
